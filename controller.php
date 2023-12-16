382118e2ef03aa1bd34e8e05c6e2906f69612653
AmeEtYuki <nicolas.varin55@gmail.com>
2023-12-16 17:43:31 +0100

    yadesphotomtn

--- controller.php
+++ controller.php
@@ -110,7 +110,11 @@ class controller {
                     // 404 ?
                 } else {
                     //on affiche tout
-                    (new vue)->pageBien($infos);
+                    $photos = (new photo)->photoPresentBien($_GET["idBien"]);
+                    $bien = $_GET['idBien'];
+                    (new vue)->pageBien(
+                        $bien, 
+                        $photos);
                 }
             } else {
                 //redirect vers accueil ?
@@ -120,7 +124,7 @@ class controller {
             //redirect vers accueil ?
             $errors[] = "Il y a une erreur sur votre lien, il manque la propriété idBien !";
         }
-        var_dump($errors);
+        //var_dump($errors);
     }
 }
 ?>
